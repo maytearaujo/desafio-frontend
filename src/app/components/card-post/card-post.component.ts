f69a69116669781a652067674244f45c39288d6c
Maytê <maytearaujo@yahoo.com.br>
2025-05-06 22:15:09 -0300

    :construction: Card post

---
+++ src/app/components/card-post/card-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-post.component.html',
+  styleUrl: './card-post.component.css'
+})
+export class CardPostComponent {
+
+}
